0aa89eaeb813ec6b2bdd9102af3f09552114c5a7
WebberLu <e1e1lu32@gmail.com>
2020-08-24 16:45:59 +0800

    use Recycleview implement https://ithelp.ithome.com.tw/articles/10187869 https://www.tpisoftware.com/tpu/articleDetails/1274

--- app/src/main/java/com/wl/recyclerviewdemo200824/MainActivity.kt
+++ app/src/main/java/com/wl/recyclerviewdemo200824/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.wl.recyclerviewdemo200824
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.wl.recyclerviewdemo200824.adapter.DataModel
+import com.wl.recyclerviewdemo200824.adapter.RecyclerViewAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mAdapter: ArrayAdapter<String>
     var mData: ArrayList<String> = ArrayList()
+    var mRecyclerViewData: ArrayList<DataModel> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +27,21 @@ class MainActivity : AppCompatActivity() {
 
         mAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mData)
 
-        listview.adapter = mAdapter
-        listview.onItemClickListener =
-                AdapterView.OnItemClickListener { _, _, i, _ ->
-                    Toast.makeText(this, "您選了 : " + mData[i], Toast.LENGTH_SHORT)
-                            .show()
+//        listview.adapter = mAdapter
+//        listview.onItemClickListener =
+//                AdapterView.OnItemClickListener { _, _, i, _ ->
+//                    Toast.makeText(this, "您選了 : " + mData[i], Toast.LENGTH_SHORT)
+//                            .show()
+//        }
+
+        for (i in 0..49) {
+            mRecyclerViewData.add(DataModel("項目$i", R.drawable.ic_launcher_foreground))
+        }
+
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = RecyclerViewAdapter(context, mRecyclerViewData)
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         }
     }
 }
