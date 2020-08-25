cc9e4483115f8ed243e75ccba3daef05f46e33f2
WebberLu <e1e1lu32@gmail.com>
2020-08-25 10:50:12 +0800

    Revert "Merge pull request #2 from WebberLu/feature0825"
    This reverts commit 1cc02aa66bcfd91d008f6384bb7be35d1ce2081d, reversing
    changes made to 48c523764be5782da76cac43227c9b36c0f2064a.

--- app/src/main/java/com/wl/recyclerviewdemo200824/MainActivity.kt
+++ app/src/main/java/com/wl/recyclerviewdemo200824/MainActivity.kt
@@ -6,18 +6,16 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wl.recyclerviewdemo200824.adapter.DataModel
 import com.wl.recyclerviewdemo200824.adapter.RecyclerViewAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(),RecyclerViewAdapter.OnItemClickHandler {
+class MainActivity : AppCompatActivity() {
 
     lateinit var mAdapter: ArrayAdapter<String>
     var mData: ArrayList<String> = ArrayList()
     var mRecyclerViewData: ArrayList<DataModel> = ArrayList()
-    var viewAdapter: RecyclerViewAdapter = RecyclerViewAdapter(mRecyclerViewData, this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,27 +39,9 @@ class MainActivity : AppCompatActivity(),RecyclerViewAdapter.OnItemClickHandler 
         }
 
         recycler_view.apply {
-            //type 1
             layoutManager = LinearLayoutManager(context)
-            //type 2
-//            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            //type 3
-//            layoutManager = GridLayoutManager(context,2)
-
-            adapter = viewAdapter
-
+            adapter = RecyclerViewAdapter(context, mRecyclerViewData)
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         }
-
-        btnAdd.setOnClickListener { _-> viewAdapter.addItem(DataModel("new item", R.drawable.ic_launcher_background))}
-    }
-
-    override fun onItemClick(position: Int, data: DataModel) {
-        Toast.makeText(this, "您選了 : " + data.txtItem, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onItemRemove(position: Int, data: DataModel) {
-        viewAdapter.removeItem(position)
-        Toast.makeText(this, "remove : " + data.txtItem, Toast.LENGTH_SHORT).show()
     }
 }
